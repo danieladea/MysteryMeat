d51bcb64351d4dab6cf8009d609e0ccc60cffc8c
Daniel Adea <danielnadea@gmail.com>
2019-02-13 18:49:06 -0800

    Initial Commit

---
+++ MysteryMeat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MysteryMeat
+//
+//  Created by Daniel Adea on 2/13/19.
+//  Copyright © 2019 Daniel Adea. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ MysteryMeatTests/MysteryMeatTests.swift
@@ -0,0 +1,34 @@
+//
+//  MysteryMeatTests.swift
+//  MysteryMeatTests
+//
+//  Created by Daniel Adea on 2/13/19.
+//  Copyright © 2019 Daniel Adea. All rights reserved.
+//
+
+import XCTest
+@testable import MysteryMeat
+
+class MysteryMeatTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
